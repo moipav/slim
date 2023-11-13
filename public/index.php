75a64fc08433331934d710a53fc4e3de104ab25e
P.M <moipav@yandex.ru>
2023-11-13 14:59:59 +0800

    add  some routes

--- public/index.php
+++ public/index.php
@@ -8,12 +8,23 @@ $app = AppFactory::create();
 // $app->addErrorMidleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to slim');
+    $response->getBody()->write('Welcome to sl1m');
     return $response;
 });
 
 $app->get('/companies', function ($request, $response) {
     $response->getBody()->write('GET /companies');
+    return $response;
+});
+
+$app->get('/users', function ($request, $response) {
+    $response->getBody()->write('GET /users');
+    return $response;
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $id = $args['id'];
+    $response->getBody()->write("My id: {$id}");
     return $response;
 });
 
